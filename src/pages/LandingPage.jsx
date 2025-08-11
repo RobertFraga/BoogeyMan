1c648b1d3c5c8049989ee36b69748de4f99c392f
William <robertfraga517@gmail.com>
2025-08-11 22:47:04 +0800

    adding about section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import Mainimg from "../assets/Version control-amico.svg";
+import about from "../assets/Version control-bro.svg";
 import Caroucel from "../components/Caroucel";
 
 const Landing = () => {
@@ -7,7 +8,7 @@ const Landing = () => {
       <main className="flex flex-col md:flex-row items-center m-5 md:ml-20 space-y-6 md:space-y-0 md:space-x-8 text-center md:text-left">
         <div className="flex-1">
           <h1
-            className="text-purple-700 font-bold leading-tight"
+            className="text-purple-700 font-black leading-tight"
             style={{
               fontSize: "clamp(2rem, 4vw + 1.5rem, 4rem)", // Fits 375px without wrap
             }}
@@ -29,6 +30,28 @@ const Landing = () => {
         </div>
       </main>
       <Caroucel />
+      <div className="relative md:flex md:m-30">
+        {/* Image */}
+        <img
+          src={about}
+          alt="about me"
+          className="w-full md:w-1/2 object-cover"
+        />
+
+        {/* Mask/Text */}
+        <div className="absolute inset-0 flex flex-col justify-center items-center bg-black/90 p-6 md:static md:w-1/2 md:bg-transparent md:items-start md:justify-center">
+          <h2 className="sm:text-purple-700 md:text-6xl text-4xl font-bold mb-4">
+            About me
+          </h2>
+          <p className="sm:text-purple-700 md:text-left text-center text-xl md:text-2xl md:font-semibold">
+            I'm a freelance web developer focused on crafting responsive,
+            high-performance websites and web apps that scale with your
+            business. From startups to solo founders or solo projects, I help
+            bring ideas to life with clean code, modern tech stacks, and
+            user-first design.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
