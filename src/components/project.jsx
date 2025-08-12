3c9e7f84e711bb9628f37274607d78265669e3ad
William <robertfraga517@gmail.com>
2025-08-12 18:45:02 +0800

    adding essensial link

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,13 +3,17 @@ import project from "../assets/Admin-amico.svg";
 const Card = (props) => {
   return (
     <>
-      <div className="bg-white flex flex-col items-center w-65 rounded-lg m-5">
-        <img src={project} className="max-w-[200px]" />
-
+      <a
+        href={props.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-white flex flex-col items-center w-65 rounded-lg m-5 hover:shadow-lg transition"
+      >
+        <img src={project} className="max-w-[200px]" alt={props.title} />
         <div className="bg-[#FFF7F7] h-20 flex items-center justify-center w-full rounded-b-lg">
           <p className="text-purple-700 text-lg">{props.title}</p>
         </div>
-      </div>
+      </a>
     </>
   );
 };
